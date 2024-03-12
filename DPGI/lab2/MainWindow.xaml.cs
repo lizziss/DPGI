072c27b992656275f05e78fd0f272e49928c9d24
Drelya Liza <lizadrelya@gmail.com>
2024-03-13 00:02:28 +0200

    Did the exercise 4

--- DPGI/lab2/MainWindow.xaml.cs
+++ DPGI/lab2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,54 @@ namespace lab2
         public MainWindow()
         {
             InitializeComponent();
+            //Створення прив'язки та приєднання обробників
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            //Реєстрація прив'язки
+            CommandBindings.Add(saveCommand);
+
+
+            // Створення прив'язки та приєднання обробників для команди Open
+            CommandBinding openCommand = new CommandBinding(ApplicationCommands.Open, execute_Open, canExecute_Open);
+            CommandBindings.Add(openCommand);
+            // Створення прив'язки та приєднання обробників для власної команди Clear
+            CommandBinding clearCommand = new CommandBinding(ApplicationCommands.Cut, execute_Clear, canExecute_Clear);
+            CommandBindings.Add(clearCommand);
+
         }
+
+        void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("D:\\ГІ\\DPGI\\DPGI\\lab2\\myFile.txt", inputTextBox.Text);
+            MessageBox.Show("The file was saved!");
+        }
+        void canExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length == 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                inputTextBox.Text = System.IO.File.ReadAllText(filePath);
+                MessageBox.Show("File opened successfully!");
+            }
+              
+        }
+
+        void canExecute_Clear(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (inputTextBox.Text.Trim().Length > 0) e.CanExecute = true; else e.CanExecute = false;
+        }
+        void execute_Clear(object sender, ExecutedRoutedEventArgs e)
+        {
+            inputTextBox.Text = "";
+        }
+
     }
 }
