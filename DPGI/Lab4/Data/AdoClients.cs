895a0570ffe3a6bc5fdea6ffbc7ec08594a93ef9
Drelya Liza <lizadrelya@gmail.com>
2024-05-07 15:21:48 +0300

    Create functional for create, update, detele in DB, change interface

--- DPGI/Lab4/Data/AdoClients.cs
+++ DPGI/Lab4/Data/AdoClients.cs
@@ -6,6 +6,10 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Security.Policy;
+using System.Net;
+using System.Xml.Linq;
 
 namespace Lab4.Data
 {
@@ -37,6 +41,92 @@ namespace Lab4.Data
             }
 
             return dataTable;
+        }
+
+        
+
+        public void UpdateClient(int id, string name, string phone, string address, decimal income, decimal expenses)
+        {
+           
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string query = "UPDATE Clients SET Name = @Name, Phone = @Phone, Address = @Address, " +
+                  "Income = @Income, Expenses = @Expenses WHERE ID = @ID";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Phone", phone);
+                    command.Parameters.AddWithValue("@Address", address);
+                    command.Parameters.AddWithValue("@Income", income);
+                    command.Parameters.AddWithValue("@Expenses", expenses);
+                    command.Parameters.AddWithValue("@ID", id);
+
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Updated successfully" ,"" ,MessageBoxButton.OK , MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error updating client data: {ex.Message}");
+                }
+
+                connection.Close();
+            }
+        }
+
+       
+        public void AddClient(string name, string phone, string address, decimal income, decimal expenses)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string query = "INSERT INTO Clients (Name, Phone, Address, Income, Expenses) " +
+                                   "VALUES (@Name, @Phone, @Address, @Income, @Expenses)";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Phone", phone);
+                    command.Parameters.AddWithValue("@Address", address);
+                    command.Parameters.AddWithValue("@Income", income);
+                    command.Parameters.AddWithValue("@Expenses", expenses);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Create successfully", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Помилка додавання клієнта: {ex.Message}");
+                }
+                connection.Close();
+            }
+        }
+
+
+        public void DeleteClient(int id)
+        {
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    string query = $"DELETE FROM Clients WHERE ID = {id}";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Delete successfully", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error deleting client: {ex.Message}");
+                }
+                connection.Close();
+            }
+
         }
     }
 
--- DPGI/Lab4/MainWindow.xaml.cs
+++ DPGI/Lab4/MainWindow.xaml.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Lab4
 {
@@ -31,11 +35,116 @@ namespace Lab4
         
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Reload();
+        }
+        private void Reload()
+        {
             listClients.Focus();
             listClients.DataContext = myTable.GetAllClients();
             listClients.SelectedIndex = 0;
         }
 
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string phone = PhoneTextBox.Text;
+            string address = AddressTextBox.Text;
+            decimal income;
+            decimal expenses;
+        
+            if (listClients.SelectedIndex != -1)
+            {
+                MessageBox.Show("Press 'Clear' button");
+                return;
+            }
+
+            // Перевірка на заповненість всіх полів
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(phone) ||
+                string.IsNullOrEmpty(address) || string.IsNullOrEmpty(IncomeTextBox.Text) ||
+                string.IsNullOrEmpty(ExpensesTextBox.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+           
+
+            // Перевірка на коректність введених значень до обробки
+            if (!decimal.TryParse(IncomeTextBox.Text.Replace('.', ','), out income) ||
+                !decimal.TryParse(ExpensesTextBox.Text.Replace('.', ','), out expenses))
+            {
+                MessageBox.Show("Income and Expenses must be decimal values.");
+                return;
+            }
+
+
+            myTable.AddClient(name, phone, address, income, expenses);
+            Reload();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            string name = NameTextBox.Text;
+            string phone = PhoneTextBox.Text;
+            string address = AddressTextBox.Text;
+            decimal income;
+            decimal expenses;
+
+            if (listClients.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select item to update");
+                return;
+            }
+
+            // Перевірка на заповненість всіх полів
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(phone) ||
+                string.IsNullOrEmpty(address) || string.IsNullOrEmpty(IncomeTextBox.Text) ||
+                string.IsNullOrEmpty(ExpensesTextBox.Text))
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+
+
+            // Перевірка на коректність введених значень до обробки
+            if (!decimal.TryParse(IncomeTextBox.Text.Replace('.', ','), out income) ||
+                !decimal.TryParse(ExpensesTextBox.Text.Replace('.', ','), out expenses) || 
+                !int.TryParse(IdTextBox.Text.Replace('.', ','), out id))
+            {
+                MessageBox.Show("Income and Expenses must be decimal values.");
+                return;
+            }
+
+
+            myTable.UpdateClient(id,name, phone, address, income, expenses);
+            Reload();
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            listClients.SelectedIndex = -1;
+            
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (listClients.SelectedIndex == -1)
+            {
+                MessageBox.Show("Оберіть продукт який необхідно видалити");
+                return;
+            }
+         
+            myTable.DeleteClient(int.Parse(IdTextBox.Text));
+            Reload();
+
+        }
+
+
+        private void TxtAmount_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9.]+").IsMatch(e.Text);
+        }
 
     }
 }
